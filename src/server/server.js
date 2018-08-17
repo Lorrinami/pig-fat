43be885cc194e857123b8124642e07838496673d
duyongkang <duyongkang@shangbin.org>
2018-08-17 17:29:53 +0800

    add cros

--- src/server/server.js
+++ src/server/server.js
@@ -7,6 +7,7 @@ var credentials = {key: privateKey, cert: certificate};
 var express = require('express');
 var app = express();
 const DATA_FILE = "data.json";
+const DATA_FILES = "datas.json";
 var moment = require('moment');
 
 app.get('/get_rollnum', function (req, res) {
@@ -24,6 +25,87 @@ app.get('/get_rollnum', function (req, res) {
         }
     });
 });
+
+app.get('/get_record_s', function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+
+    fs.readFile(DATA_FILES, function (err, data) {
+        if (err) {
+            console.error(err);
+        } else {
+            const parseData = JSON.parse(data);
+            res.send(parseData);
+        }
+    });
+});
+
+app.get('/roll_s', function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+
+    let rand = Math.random() * 1000
+    let goodId = 9
+    if (rand >= 0 && rand <= 20) {
+        goodId = 1
+
+    } else if (rand > 20 && rand <= 80) {
+        goodId = 2
+
+    } else if (rand > 80 && rand <= 180) {
+        goodId = 3
+
+    } else if (rand > 180 && rand <= 330) {
+        goodId = 4
+
+    } else if (rand > 330 && rand <= 480) {
+        goodId = 5
+
+    } else if (rand > 480 && rand <= 600) {
+        goodId = 6
+
+    } else if (rand > 600 && rand <= 730) {
+        goodId = 7
+
+    } else if (rand > 730 && rand <= 860) {
+        goodId = 8
+
+    } else if (rand > 860 && rand <= 1000) {
+        goodId = 9
+
+    }
+
+    fs.readFile(DATA_FILES, function (err, data) {
+        if (err) {
+            console.error(err);
+        } else {
+            let timeStamp = moment();
+            // let year = moment("2018-08-16T09:33:17.267Z").format('YYYY.MM.DD');
+            // let time = (moment("2018-08-16T09:33:17.267Z").format('A') === 'AM' ? '上午' : '下午') + moment("2018-08-16T09:33:17.267Z").format('h:mm');
+            // console.log(year + " " + time);
+            const parseData = JSON.parse(data);
+
+            if (parseData.rollNumber > 0) {
+                parseData.rollNumber -= 1
+                parseData.record.push({goodId: goodId, time_stamp: timeStamp, used: false})
+
+                fs.writeFile(DATA_FILES, JSON.stringify(parseData), err => {
+                    if (err) throw err;
+                    res.send(parseData);
+                });
+            } else {
+                res.send({rollNumber: 0, err: -1})
+            }
+
+
+        }
+    });
+});
+
 
 app.get('/get_record', function (req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
